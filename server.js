2c86ae45583713f188c74df084b1ef1bcc429ce5
sukhad123 <sukhadadhikari3@gmail.com>
2024-02-01 23:19:39 -0500

    final touches

--- server.js
+++ server.js
@@ -28,6 +28,11 @@ app.use(express.urlencoded({ extended: true }));
 require('dotenv').config();
  
 
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
+
+
 const clientSessions = require('client-sessions');
 app.use(
   clientSessions({
